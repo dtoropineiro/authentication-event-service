28649beb34090969c019bf0d5a071e7c9f2056ea
dtoropineiro <dtoropineiro@gmail.com>
2020-12-11 14:32:17 -0300

    Added event routes

---
+++ routes/events.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/field-validator')
+const { validateJwt } = require('../middlewares/jwtValidator');
+const { isDate } = require('../middlewares/date-validator');
+const { createEvent, getEvents, updateEvent, deleteEvent } = require('../controllers/events')
+const router = Router();
+
+router.use(validateJwt);
+
+router.post('/',
+    [
+        check('title', 'title required.').not().isEmpty(),
+        check('start', 'Invalid date.').custom(isDate),
+        check('end', 'Invalid date.').custom(isDate),
+        validateFields
+    ],
+    createEvent);
+router.get('/', getEvents);
+router.put('/:id', updateEvent);
+router.delete('/:id', deleteEvent);
+
+module.exports = router;
